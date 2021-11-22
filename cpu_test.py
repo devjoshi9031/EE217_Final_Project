a61484c4fb4e3e0ecd6cf01dac52d2d3a9bee8bc
Dev Joshi <devjoshi9031@gmail.com>
2021-11-21 17:08:32 -0800

    Adapted code to calculate SVD and reconstruct the image for more than one image

--- cpu_test.py
+++ cpu_test.py
@@ -4,38 +4,47 @@ import os
 import matplotlib.pyplot as plt
 import time
 # number of rows and columns taken to reconstruct the image.
-recon_rank=200
+recon_rank=100
+# Name of the folder that contains all the images.
+path = 'images/'
+# Get the name of all the files inside the folder in an array.
+list_of_images = os.listdir(path)
 
-check_point1 = time.time()
-# Get the image from the current directory.
-image = plt.imread("/home/dev/EE217_Final_Project/test2.jpg")
-# Convert the coloured image into grayscale image
-image = np.mean(image,-1)
-# Perform Singular value decomposition of the image
-U, S, V = np.linalg.svd(image, full_matrices=False)
+# Run SVD for all the files inside a folder.
+total_time = 0
+for file in range(0,len(list_of_images)):
 
-# Reconstruct the image using "recon_rank" number of rows and columns from U, S, and V matrices.
-ret = U[:,:recon_rank] @ np.diag(S)[0:recon_rank, :recon_rank] @ V[:recon_rank,:]
+    check_point1 = time.time()
+    # Get the image from the current directory and read it in grayscale.
+    image = cv2.imread(os.path.join(path, list_of_images[file]),0)
+    # Convert the coloured image into grayscale image
+    # image = np.mean(image,-1)
+    # Perform Singular value decomposition of the image
+    U, S, V = np.linalg.svd(image, full_matrices=False)
 
-check_point2 = time.time()
-print("Time taken to calculate SVD and reconstruct image: "+str(check_point2-check_point1))
+    # Reconstruct the image using "recon_rank" number of rows and columns from U, S, and V matrices.
+    ret = U[:,:recon_rank] @ np.diag(S)[0:recon_rank, :recon_rank] @ V[:recon_rank,:]
 
-# Calculate the difference between the reconstructed image and original image.
-diff_array = np.subtract(image, ret)
+    check_point2 = time.time()
+    total_time +=(check_point2-check_point1)
+    print("Time taken to calculate SVD and reconstruct image for the file number " +str(file)+" is: "+str(check_point2-check_point1)+ 'secs')
 
-# Plot both the figures side-by-side
-fig = plt.figure()
-ax1 = fig.add_subplot(1,2,1)
-img = ax1.imshow(ret)
-img.set_cmap('gray')
-ax1.title.set_text('SVD with reconstruction using:'+str(recon_rank))
-plt.axis('off')
+    # Calculate the difference between the reconstructed image and original image.
+    diff_array = np.subtract(image, ret)
 
-ax2 = fig.add_subplot(1,2,2)
-img = ax2.imshow(image)
-img.set_cmap('gray')
-ax2.title.set_text('Original Image')
-plt.axis('off')
-plt.show()
+    # Plot both the figures side-by-side
+    fig = plt.figure()
+    ax1 = fig.add_subplot(1,2,1)
+    img = ax1.imshow(ret)
+    img.set_cmap('gray')
+    ax1.title.set_text('SVD with reconstruction using:'+str(recon_rank))
+    plt.axis('off')
 
+    ax2 = fig.add_subplot(1,2,2)
+    img = ax2.imshow(image)
+    img.set_cmap('gray')
+    ax2.title.set_text('Original Image')
+    plt.axis('off')
+    plt.show()
 
+print("Time taken to calculate SVD and reconstruct image for "+str(len(list_of_images))+" number of files is: "+str(total_time))
